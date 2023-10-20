f1fe7a7650ace55a2a3ce2d572246fd090faa50f
khameer_13 <khameer2002@gmail.com>
2023-10-20 21:49:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+
+function handleKeyPress(event) {
+  if (event.key === 'Enter') {
+    addingcontacts();
+  }
+}
+function addingcontacts() {
+  const inputdata = document.getElementById('data').value;
+  if (inputdata.length >= 1) {
+    const li_new = document.createElement("li");
+    li_new.innerHTML = inputdata;
+    document.getElementById("listofcontacts").appendChild(li_new);
+    document.getElementById('data').value = "";
+  }
+  else {
+    alert("enter a name or number")
+  };
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='flexing'>
+        <div className='contactBox'>
+
+          <h1 className='heading'>
+            contact List
+          </h1>
+
+          < div className='contactlist'>
+            <ol id="listofcontacts">
+
+            </ol>
+          </div>
+
+          <div className='inputBox'>
+            <input id="data" type="text" placeholder="Enter your text here" onKeyPress={handleKeyPress} />
+          </div>
+
+          <button type="button" onClick={addingcontacts}> save </button>
+
+        </div>
+
+      </div>
+
+
+    </>
   );
 }
 
